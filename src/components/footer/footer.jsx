6a6c1de3462d9477025c5b3b998a16b665bcf735
srinivasan <srinivasan@pop-os.localdomain>
2023-08-08 18:44:00 +0530

    added footer and nav

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,79 @@
+import react from "react";
+import "./footer.css"
+const Footer=()=>{
+    return(
+      
+  <footer className="footer-main bg-dark fixed-bottom">
+  <div className="container">
+    <div className="row address-main">
+      <div className="col-lg-4 col-sm-12 col-xs-12">
+        <div className="address-box clearfix">
+          <div className="add-icon">
+            <img src="Img/footer-icon-01.png" alt="" />
+          </div>
+          <div className="add-content">
+            <h5>Address</h5>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+			sed do eiusmod tempor incididunt ut veniam </p>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-4 col-sm-12 col-xs-12">
+        <div className="address-box clearfix">
+          <div className="add-icon">
+            <img src="Img/footer-icon-02.png" alt="" />
+          </div>
+          <div className="add-content">
+            <h5>Phone</h5>
+            <p>  +(91) 000000000 <br />
+            +(91) 000000000  </p>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-4 col-sm-12 col-xs-12">
+        <div className="address-box clearfix">
+          <div className="add-icon" >
+            <img src={"Img/footer-icon-03.png"} alt="" />
+          </div>
+          <div className="add-content">
+            <h5>Email</h5>
+            <p> <a href="mailto:" style={{textDecoration:"none"}}>collab@codewithfaraz.com</a> </p>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+
+
+<footer className="bg-dark text-center text-white">
+
+<div className="container p-4 pb-0">
+  <section className="mb-">
+    <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      ><i className="fa fa-facebook-f"></i
+    ></a>
+
+    <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      ><i className="fa fa-twitter"></i
+    ></a>
+
+    <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      ><i className="fa fa-google"></i
+    ></a>
+
+    <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      ><i className="fa fa-instagram"></i
+    ></a>
+
+  </section>
+</div>
+
+<div className="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
+codewithfaraz © 2023 All Rights Reserved.
+ 
+                     </div>
+</footer>
+</footer>
+
+)}
+export default Footer;
---
+++ src/components/navbar/nav.jsx
@@ -0,0 +1,52 @@
+import "./nav.css"
+import { Fragment } from "react";
+import logo from './static/hack-logo.png'
+const Navbar=()=>{
+    
+    return(
+        <Fragment>
+        <nav class="header-nav">
+        <div class="branding">
+            <img src={logo} alt=""/>
+            {/* <h1 class="brand-name">Lync Fest</h1> */}
+        </div>
+
+        <div class="nav-buttons">
+            <a href="#hero" class="nav-btn">Home</a>
+            <a href="https://ashik-irfan.github.io/lyncfestbuild" class="nav-btn">Hackathon</a>
+            <a href="#sponsors" class="nav-btn">Sponsors</a>
+            <a href="#about-us" class="nav-btn">About</a>
+            <a href="#footer" class="nav-btn">Contact</a>
+            <a href="#events" class="nav-btn ghost-btn">Events</a>
+        </div>
+    
+    </nav>
+
+    <div class="nav-buttons-mobile hidden-nav">
+        <a href="#hero" class="nav-btn">Home</a>
+        <a href="https://ashik-irfan.github.io/lyncfestbuild" class="nav-btn">Hackathon</a>
+        <a href="#sponsors" class="nav-btn">Sponsors</a>
+        <a href="#about-us" class="nav-btn">About</a>
+        <a href="#footer" class="nav-btn">Contact</a>
+        <a href="#events" class="nav-btn ghost-btn">Events</a>
+    </div>
+
+    <div class="hamburger" onClick={(e)=>{
+            document.querySelector('.nav-buttons-mobile').classList.toggle('hidden-nav')
+            let i=0;
+            document.querySelectorAll('.hamburger span').forEach((span)=>{ 
+              i+=1; 
+              span.classList.toggle(`close-${i}`);
+             })
+          }
+    }>
+        <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span>
+    </div>
+
+    </Fragment>
+    )   
+
+}
+export default Navbar;
