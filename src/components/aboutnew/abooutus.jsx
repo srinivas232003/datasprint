10eb0a7121ad1d2ede52f6d3c8e1e0da7874ee8f
srinivasan <srinivasan@pop-os.localdomain>
2023-08-18 19:48:24 +0530

    prod release 3

--- src/components/aboutnew/abooutus.jsx
+++ src/components/aboutnew/abooutus.jsx
@@ -5,7 +5,7 @@ const Aboutus=()=>{
         <div class="abt-us">
             <h1 style={{"text-align":"center","color":"rgb(191, 101, 191)"}}>About us</h1>
             <div className="abt-cnt">
-            <div>Welcome to the Department of Artificial Intelligence and Data Science of Sri Sai Ram Institute of Technology , a realm where human ingenuity converges with cutting-edge technology to shape the future. Our department is a hub of innovation, exploration, and transformation, dedicated to unraveling the mysteries of Artificial Intelligence and harnessing its vast potential to revolutionize industries, enhance human capabilities, and drive progress.</div>
+            <div>Welcome to the Department of Artificial Intelligence and Data Science of Sri Sairam Institute of Technology , a realm where human ingenuity converges with cutting-edge technology to shape the future. Our department is a hub of innovation, exploration, and transformation, dedicated to unraveling the mysteries of Artificial Intelligence and harnessing its vast potential to revolutionize industries, enhance human capabilities, and drive progress.</div>
 <br/>
 <div>In our department, we are at the forefront of the AI revolution, pushing boundaries, and redefining what's possible. Our team of faculties and experts collaborate to further research on AI systems that can reason, learn, and adapt, mimicking human intelligence while harnessing the power of computational prowess. The department also focuses on advancements of the domain with collaboration with in College Professional Societies such as the Institute of Electronics annd Electrical Engineering (IEEE).</div>
 <br/>
