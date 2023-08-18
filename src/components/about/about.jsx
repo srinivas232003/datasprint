1716e609f9c5422c4d79f55bd78d50b27ae404c3
srinivasan <srinivasan@pop-os.localdomain>
2023-08-19 02:23:24 +0530

    about us added

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,7 +3,6 @@ import'./about.css'
 const About=()=>{
     return(
     <section className="about-us" id="about-us">
-        <hr />
         <h1>About DataSprint</h1>
         <div>Are you ready to embark on a data-driven adventure? DataSprint is your chance to immerse yourself in the world of AI and data science. Whether you're a coding prodigy or a creative thinker, this hackathon offers a platform to showcase your skills, learn from industry exports, and collaborate with like-minded enthusiasts.
 <br/>
--- src/components/aboutnew/abooutus.jsx
+++ src/components/aboutnew/abooutus.jsx
@@ -3,7 +3,7 @@ import "./aboutus.css"
 const Aboutus=()=>{
     return(
         <div class="abt-us">
-            <h1 style={{"text-align":"center","color":"rgb(191, 101, 191)"}}>About us</h1>
+            {/* <h1 style={{"text-align":"center","color":"rgb(191, 101, 191)"}}>About us</h1>
             <div className="abt-cnt">
             <div>Welcome to the Department of Artificial Intelligence and Data Science of Sri Sairam Institute of Technology , a realm where human ingenuity converges with cutting-edge technology to shape the future. Our department is a hub of innovation, exploration, and transformation, dedicated to unraveling the mysteries of Artificial Intelligence and harnessing its vast potential to revolutionize industries, enhance human capabilities, and drive progress.</div>
 <br/>
@@ -12,6 +12,28 @@ const Aboutus=()=>{
 <div>
 As you step into the Department of Artificial Intelligence, prepare to embark on a voyage of discovery, innovation, and transformation. Whether you're a student, a researcher, an industry professional, or simply an AI enthusiast, we invite you to join us in shaping a world where machines and humans coexist in harmony, forging a brighter, more intelligent future for all.
 </div>
+</div> */}
+<div class="responsive-container-block bigContainer">
+  <div class="responsive-container-block Container bottomContainer">
+    <div class="ultimateImg">
+      <img class="mainImg" src="https://res.cloudinary.com/dcsduqstu/image/upload/v1692385739/sairamit-ai-ds-20overview-623x288_zxh0kf.jpg"/>
+      <div class="purpleBox">
+<img className="mini" src="https://res.cloudinary.com/dcsduqstu/image/upload/v1692376767/SIT_-_LOGO_WHITE_gkz6xk.png"></img>        
+      </div>
+    </div>
+    <div class="allText bottomText">
+      <p class="text-blk headingText">
+        About us
+      </p>
+      <p class="text-blk subHeadingText">
+      Shaping Tomorrow with AI and Data Science </p>
+      <p class="text-blk description">
+      Welcome to the heart of innovation and exploration - the Department of <span class="ai">Artificial Intelligence and Data Science </span> Sri Sairam Institute of Technology. Our dedicated team of experts and visionary faculty members drive forward the AI revolution, researching systems that emulate human intelligence while harnessing computational power. With a focus on collaboration and industry advancements, we are crafting a future where humans and machines thrive together. Join us in pioneering the next era of intelligence and progress.      </p>
+      <a href="http://ai.sairamit.edu.in/" class="">
+          <button className="butn">Visit us</button>
+      </a>
+    </div>
+  </div>
 </div>
         </div>
     )
