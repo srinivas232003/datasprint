04dcc7f1e5bbd608107901655c2e18662339a2ff
srinivasan <srinivasan@pop-os.localdomain>
2023-08-31 10:07:39 +0530

    registration closed

--- src/components/forms/forms.jsx
+++ src/components/forms/forms.jsx
@@ -3,8 +3,9 @@ import "./forms.css"
 const Form=()=>{
 return(
     <div className="frm" >
-
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfkdgaqqpREZV9McURGSppfHuU-nZSZT7O0AID3-GQLx5dbQw/viewform?embedded=true" width="100%" height="2200" scrolling="yes" frameBorder={"0"} marginHeight="0" marginWidth="0" target="_self">Loading…</iframe>
+<div className="txtbx"> 
+    Registration is now closed.
+</div>
     </div>
 )
 }
